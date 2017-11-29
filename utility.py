3c0aa576349aa81b8c362c3b1863874aaee158d5
XiaoXiaoqian <xiaoqian@stanford.edu>
2017-11-29 10:33:56 -0800

    add untar function in utility

--- utility.py
+++ utility.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python2
 """
 @author: Xiaoqian Xiao
-
 Created on Mon Nov 27 16:14:44 2017
 """
-
+%reset
 import os, sys, tarfile
-
-def extract(tar_url, extract_path='.'):
-    print tar_url
-    #tar = tarfile.open(tar_url, 'r')
-    for item in tar:
-        #tar.extract(item, extract_path)
-        if item.name.find(".tgz") != -1 or item.name.find(".tar") != -1:
-            tar.extract(item, extract_path + "/" + item.name[:item.name.rfind('/')])
+### fix the problem with subdir
+def extract(root, extract_path='.'):   
+    for path, subdirs, files in os.walk(root):
+        for filename in files:
+            filepath = os.path.join(path, filename)
+            if filepath is root:
+                resultpath = extract_path
+            else:
+                resultpath = os.path.join(extract_path, path[path.rfind(root)+len(root)+1:])
+            print resultpath
+            if not os.path.isdir(resultpath):
+                os.makedirs(resultpath)
+            if tarfile.is_tarfile(filepath):
+                tar = tarfile.open(filepath, 'r:*')
+                for member in tar:
+                    tar.extract(member, resultpath)
+            
