86dd67badeef157f729f1249ab6441d5a81a9214
XiaoXiaoqian <33399278+XiaoXiaoqian@users.noreply.github.com>
2018-02-05 10:44:57 -0800

    Update bids.py

--- bids.py
+++ bids.py
@@ -3,9 +3,17 @@
 @author: Xiaoqian Xiao
 Deal with transferring to BIDS
 **refer heudiconv
-"""
 Created on Mon Nov 27 13:33:34 2017
-
 """
+from dicom_xxq import *
+from utility import *
+basedir = '/Users/xiaoqian/Projects'
 
-'ManufacturersModelName', # 9 Manufacturer`s model name of the equipment that produced the composite instances. Corresponds to DICOM Tag 0008, 1090 “Manufacturers Model Name”
+basedir = '/Users/xiaoqian/Box/Xiaoqian_Externally_Shareable_Files/Projects'
+ProjectName = 'aTBS'
+rawdir = os.path.join(basedir, ProjectName, 'raw')
+untardir = os.path.join(basedir, ProjectName, 'untar')
+
+untardir1 = os.path.join(basedir, ProjectName,'untar/20171218_0653_16703')
+#untar_tgz(rawdir, untardir)
+get_scan_info(untardir1, basedir)
