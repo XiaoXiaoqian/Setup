41c4acd88f1fef8fcbd442d992f790f1bc81f032
XiaoXiaoqian <33399278+XiaoXiaoqian@users.noreply.github.com>
2018-03-05 16:57:59 -0800

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -13,10 +13,16 @@ import os.path as op
 from glob import glob
 
 remotehost = 'xiaoqian@cnic22.stanford.edu'
-remotedir = '/home/xiaoqian/Documents/nimsfs/raw/nolanw/atbs'
+#remotedir = '/home/xiaoqian/Documents/nimsfs/raw/nolanw/atbs'
 localdir = '/Users/xiaoqian/Projects/aTBS/raw'
 
-for i in dict_subID:
-    print (dict_subID[i])
-    filename = os.path.join(remotedir, i+'*')
-    os.system('scp -r "%s:%s" "%s"' % (remotehost, filename, localdir))
+#for i in dict_subID:
+#    print (dict_subID[i])
+#    filename = os.path.join(remotedir, i+'*')
+#    os.system('scp -r "%s:%s" "%s"' % (remotehost, filename, localdir))
+
+#two data sets were putting into open_atbs:20171208_0637_16657  20171209_0700_16665
+remotedir = '/home/xiaoqian/Documents/nimsfs/raw/nolanw/open_atbs'
+sfile = '20171208_0637_16657'
+filename = os.path.join(remotedir, sfile)
+os.system('scp -r "%s:%s" "%s"' % (remotehost, filename, localdir))
