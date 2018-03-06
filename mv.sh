70c890c87ad8046e65ec42277bfdacf6b5fbdeed
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2018-03-06 10:16:20 -0800

    Create mv.sh

---
+++ mv.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+basedir=/Users/xiaoqian/Projects/aTBS/raw
+cd $basedir
+for fdir in 20*
+do 
+  echo ${fdir}
+  cd ${fdir}
+  sudo rm -rf *Save *Shim *calibration *fgre
+  for ffdir in 1*
+  do
+    sudo rm -rf *physio* *pfile* *pyrdb
+  done
+done
