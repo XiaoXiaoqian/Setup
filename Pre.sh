f6cf1398482034c3583a444812240ba8629153e0
XiaoXiaoqian <33399278+XiaoXiaoqian@users.noreply.github.com>
2018-03-16 09:07:13 -0700

    Update Pre.sh

--- Pre.sh
+++ Pre.sh
@@ -4,7 +4,8 @@ DataPath='/Users/xiaoqian/Projects/aTBS/source'
 WorkPath='/Users/xiaoqian/Projects/aTBS/work'
 NipVersion='latest'
 
-for sub in 1 2 3 4 11 14 16 18 19 21 24 26 28
+#for sub in 1 2 3 4 11 14 16 18 19 21 24 26 28
+for sub in 2 3 4 11 14 16 18 19 21 24 26
 do 
   if [ ${sub} -lt 10];
   then
@@ -13,7 +14,7 @@ do
     subID=0${sub}
   fi
     #with results in T1 space (specitally for ICA-AROMA in T1 space)
-    #docker run -ti --rm -v $DataPath:/data:ro -v $OutputPath:/out  -v $WorkPath:/work -v $FreesuferLicsensePath/license.txt:/opt/freesurfer/license.txt poldracklab/fmriprep:$NipVersion /data /out/out participant --ignore fieldmaps --ignore slicetiming --longitudinal --participant_label sub-$subID --use-aroma --fs-license-file /opt/freesurfer/license.txt --resource-monitor -w /work --low-mem —nthreads 16 --omp-nthreads 16 --output-space T1w template
+    #docker run -ti --rm -v $DataPath:/data:ro -v $OutputPath:/out -v $WorkPath:/work -v $FreesuferLicsensePath/license.txt:/opt/freesurfer/license.txt poldracklab/fmriprep:$NipVersion /data /out/out participant --ignore fieldmaps --ignore slicetiming --longitudinal --participant_label sub-$subID --use-aroma --fs-license-file /opt/freesurfer/license.txt --resource-monitor -w /work --low-mem —nthreads 16 --omp-nthreads 16 --output-space T1w template
     #without results in T1 space (will need non-aggressive ICA-AROMA in T1 space mannually latter)
-    docker run -ti --rm -v $DataPath:/data:ro -v $OutputPath:/out  -v $WorkPath:/work -v $FreesuferLicsensePath/license.txt:/opt/freesurfer/license.txt poldracklab/fmriprep:$NipVersion /data /out/out participant --ignore fieldmaps --ignore slicetiming --longitudinal --participant_label sub-$subID --use-aroma --fs-license-file /opt/freesurfer/license.txt --resource-monitor -w /work --low-mem —nthreads 16 --omp-nthreads 16 --output-space T1w template
+    docker run -ti --rm -v $DataPath:/data:ro -v $OutputPath:/out -v $WorkPath:/work -v $FreesuferLicsensePath/license.txt:/opt/freesurfer/license.txt poldracklab/fmriprep:$NipVersion /data /out/out participant --ignore fieldmaps --ignore slicetiming --longitudinal --participant_label sub-$subID --use-aroma --fs-license-file /opt/freesurfer/license.txt --resource-monitor -w /work --low-mem —nthreads 24 --omp-nthreads 24 --output-space
 done
