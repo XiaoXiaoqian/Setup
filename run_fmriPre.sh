3f85de744875f17c5ccdc6e67dce4ccb9c3c1402
XiaoXiaoqian <33399278+XiaoXiaoqian@users.noreply.github.com>
2018-04-06 23:40:12 -0700

    Update run_fmriPre.sh

--- run_fmriPre.sh
+++ run_fmriPre.sh
@@ -2,5 +2,6 @@ baseDir='/Users/xiaoqian/Projects/aTBS'
 dataDir=${baseDir}/source
 outputDir=${baseDir}/derivatives
 workDir=${baseDir}/work
-freesurferDir='/Users/xiaoqian/tools/freesurfer'
-fmriprep $dataDir $outputDir participant --participant-label sub-$subID --ignore fieldmaps --ignore slicetiming --longitudinal --use-aroma --fs-license-file ${freesurferDir}/license.txt --resource-monitor -w $workDir --low-mem --nthreads 24 --omp-nthreads 24
+#freesurferDir='/Users/xiaoqian/tools/freesurfer'
+freesurferDir=$FREESURFER_HOME
+fmriprep $dataDir $outputDir participant --participant-label $subID --ignore fieldmaps --ignore slicetiming --longitudinal --use-aroma --fs-license-file ${freesurferDir}/license.txt --resource-monitor -w $workDir --low-mem --nthreads 24 --omp-nthreads 24
